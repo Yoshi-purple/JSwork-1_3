b2c557a03accd3e4cbd10ef3d1627c2f13e55a1b
Yoshi-purple <n7ydahew@gmail.com>
2020-12-07 19:09:08 +0900

    タスク一覧の実装をしました。

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 /** @format */
+'use strict';
+
+{
+  const tasks = ['掃除', '買い物', '散歩'];
+
+  const title = function () {
+    console.log('==================');
+    console.log('現在持っているタスク');
+    console.log('==================');
+  };
+  function lists() {
+    for (let i = 0; i < tasks.length; i++) {
+      console.log(`${i} : ${tasks[i]}`);
+    }
+  }
+  title();
+  lists();
+  const task = prompt('タスクを入力してください');
+  function addList() {
+    tasks.push(task);
+    title();
+    lists();
+    alert('新しいタスクを追加しました。');
+    prompt('「確認、追加、削除、終了」のいずれかを入力してください');
+  }
+  addList();
+}
